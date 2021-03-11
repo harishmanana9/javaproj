5dc59f905f705a15f3823e4b486fdbeeef341c09
Harish Manana <harish.manana@stackroute.in>
2021-03-11 12:01:23 -0500

    First Commit

---
+++ src/FirstPrg.java
@@ -0,0 +1,23 @@
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+//* * * * * *
+
+public class FirstPrg {
+	public static void main(String[] args) {
+		int row=0;
+		
+		for(row=1; row<=6; row++) {
+			for(int space=row; space<=6; space++) {
+				System.out.print(" ");
+			}
+			
+			for(int col=1; col<=row; col++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	}
+}
